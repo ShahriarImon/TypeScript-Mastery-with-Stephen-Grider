c7999e64cfe9b8c7e59131ba7efdb9efdd0871cf
ShahriarImon <imon.shahriar012@gmail.com>
2025-03-08 14:55:30 +0600

    created an google api key for google maps and added to script tag in index.html

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,4 +1,7 @@
+import { Company } from "./Company";
 import { User } from "./User";
 
 const user = new User();
+const company = new Company();
+console.log("company:", company);
 console.log("user:", user);
