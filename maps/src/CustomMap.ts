365c8ab2acd7cd59d3f6714169a50d8d8967f543
ShahriarImon <shahriar.imon@shebatech.com.bd>
2025-03-10 15:03:33 +0600

    map integration done

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,33 @@
+interface Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable) {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: `Hello ${mappable?.name}`,
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
