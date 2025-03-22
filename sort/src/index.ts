9df7f090ddda42a2c312d65cc53d47a231eb7d23
ShahriarImon <imon.shahriar012@gmail.com>
2025-03-22 12:25:22 +0600

    changes sorter class to abstract class and make sorting more easier

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,51 +1,31 @@
 import { CharCollection } from "./charCollection";
-import { ListCollection, Node } from "./linkedListColllection";
+import { ListCollection } from "./linkedListColllection";
 import { NumberCollection } from "./numberCollection";
 
-interface Sortable {
-  length: number;
-  swap(l: number, r: number): void;
-  compare(l: number, r: number): boolean;
-}
-
-class Sorter {
-  constructor(public collection: Sortable) {}
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
-        }
-      }
-    }
-  }
-}
+// ...........................................NumberCollection....................................................
 
 const numCollection = new NumberCollection([12, 3, 4, 0, -2, 21]);
-const num2Collection = new CharCollection("sadwerere");
-const numSorter = new Sorter(numCollection);
-const num2Sorter = new Sorter(num2Collection);
-numSorter.sort();
-num2Sorter.sort();
+numCollection.sort();
 console.log(numCollection.data);
+
+// ...........................................CharCollection....................................................
+
+const num2Collection = new CharCollection("sadwerere");
+num2Collection.sort();
 console.log(num2Collection.data);
 
-const node = new Node(10);
+// ...........................................LinkedListCollection....................................................
 
-const listCollection = new ListCollection(node);
-listCollection.add(20);
-listCollection.add(30);
-listCollection.add(40);
+const listCollection = new ListCollection();
 listCollection.add(50);
-console.log(listCollection.at(2));
+listCollection.add(40);
+listCollection.add(30);
+listCollection.add(20);
+listCollection.print();
+listCollection.insertAt(22, 3);
+listCollection.print();
+listCollection.deleteAt(1);
+listCollection.print();
 
-let c = 5;
-let d = c;
-c = 10;
-console.log(c, d);
-
-let a = { name: "ratul" };
-let b = a;
-b.name = "niloy";
-console.log(a, b);
+listCollection.sort();
+listCollection.print();
--- sort/src/linkedListColllection.ts
+++ sort/src/linkedListColllection.ts
@@ -1,13 +1,15 @@
+import { Sorter } from "./sorter";
+
 export class Node {
   constructor(public data: number) {}
   next: Node | null = null;
 }
 
-export class ListCollection {
+export class ListCollection extends Sorter {
   head: Node | null = null;
-  constructor(data: Node) {
-    this.head = data;
-  }
+  // constructor(data: Node) {
+  //   this.head = data;
+  // }
   add(data: number) {
     const node = new Node(data);
     if (!this.head) {
@@ -21,7 +23,7 @@ export class ListCollection {
     }
     tail.next = node;
   }
-  length() {
+  get length() {
     if (!this.head) {
       return 0;
     }
@@ -37,15 +39,56 @@ export class ListCollection {
     if (!this.head) {
       throw new Error("Index out of bounds");
     }
-    let node = this.head;
+    let node: Node | null = this.head;
     let counter = 0;
-    while (node.next) {
+    while (node) {
       if (index == counter) {
         return node;
       }
       counter++;
       node = node.next;
     }
-    throw new Error("Index out of bounds");
+    throw new Error("Index out of bounds" + ` length - ${this.length}`);
+  }
+
+  insertAt(value: number, index: number) {
+    if (index > 0 && index < this.length) {
+      let node = new Node(value);
+      node.next = this.at(index);
+      this.at(index - 1).next = node;
+    } else {
+      throw new Error("Index out of bounds");
+    }
+  }
+  deleteAt(index: number) {
+    if (index > 0 && index < this.length) {
+      this.at(index - 1).next = this.at(index).next;
+    } else {
+      throw new Error("Index out of bounds");
+    }
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number) {
+    let temp = this.at(leftIndex).data;
+    this.at(leftIndex).data = this.at(rightIndex).data;
+    this.at(rightIndex).data = temp;
+  }
+
+  print() {
+    if (!this.head) {
+      throw new Error("Index out of bounds");
+    }
+    let node: Node | null = this.head;
+    let printedStr = "";
+
+    while (node) {
+      printedStr = printedStr + node.data + " ";
+      node = node.next;
+    }
+    console.log(printedStr);
   }
 }
